70a1fe9a7ea775f2fa008e91a1dab6b3a99bbb16
yaoandy107 <yaoandy107@gmail.com>
2019-10-16 15:58:17 +0800

    Fix the as_dict func in project's model

--- app/model/project.py
+++ app/model/project.py
@@ -24,4 +24,4 @@ class Project(db.Model):
     user = relationship('User')
 
     def as_dict(self):
-        return {c.name: str(getattr(self, c.name)) for c in self.__table__.columns}
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
